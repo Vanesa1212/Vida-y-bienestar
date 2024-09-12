f358a87523962002bab73efeacd57c5d0815d6e3
Vanesa1212 <142362434+Vanesa1212@users.noreply.github.com>
2024-09-12 15:12:48 -0500

    actualizacion de la base de datos

--- salud_futura/settings.py
+++ salud_futura/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'salud_futura.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'salud_futura',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
+
     }
 }
 
